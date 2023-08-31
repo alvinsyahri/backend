a544e1d6c37c9d00b79c1c884bb8f34b23749fd4
alvinsyahri <alvinsyahri080@gmail.com>
2023-08-31 12:31:46 +0700

    u

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
-  secret: 'secret',
+  secret: 'session',
   resave: false,
   saveUninitialized:false,
   cookie: {
