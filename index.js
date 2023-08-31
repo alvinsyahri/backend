f868b12909045ced904f02c49102e65047b57891
alvinsyahri <alvinsyahri080@gmail.com>
2023-08-31 10:59:29 +0700

    b

--- index.js
+++ index.js
@@ -23,11 +23,6 @@ mongoose.connect(url, {
   console.log("error", err)
 })
 
-app.use(cors({
-  origin: ["*"],
-  methods: ["POST", "GET", "DELETE", "PATCH", "PUT"],
-  credentials: true
-}));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
