a8c763cf2bc8dbaf94b53a2e4f4c81952f5929ce
alvinsyahri <alvinsyahri080@gmail.com>
2023-08-31 11:16:54 +0700

    d

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(url, {
 })
 
 app.use(cors({
-  origin: ["https://inflow.my.id"],
+  origin: ["https://frontend-xi-seven.vercel.app"],
   methods: ["POST", "GET", "DELETE", "PATCH", "PUT"],
   credentials: true
 }));
