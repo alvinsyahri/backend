f1e0990235c321523585e9c485b22fe06848943e
alvinsyahri <alvinsyahri080@gmail.com>
2023-08-31 09:42:40 +0700

    tes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(url, {
 })
 
 app.use(cors({
-  origin: ["https://inflow.my.id"],
+  origin: ["http://inflow.my.id"],
   methods: ["POST", "GET", "DELETE", "PATCH", "PUT"],
   credentials: true
 }));
